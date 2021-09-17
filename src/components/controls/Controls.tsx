7eac675eecee6edef44ed4cc8b92c8f8906ea3f0
Dmitri Lisokonov <Dmitri.lisokonov@gmail.com>
2021-09-17 14:35:29 +0200

    fixed controls section layout

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -9,32 +9,41 @@ const Controls = () => {
     };
 
     return (
-        <div className="form-wrapper">
-            <div className="form flex-item">
-                <FormControl fullWidth className="attack-selection">
-                    <InputLabel id="demo-simple-select-label">Select Attack Type</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={[]}
-                        label="Age"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={10}>DDOS</MenuItem>
-                        <MenuItem value={20}>SSH Bruteforce</MenuItem>
-                        <MenuItem value={30}>Man in the Middle</MenuItem>
-                    </Select>
-                </FormControl>
+        <div className="upper">
+            <div className="form-wrapper">
+                <div className="form flex-item">
+                    <div className="lmfao">
+                        <FormControl fullWidth className="attack-selection">
+                            <InputLabel id="demo-simple-select-label">Select Attack Type</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={[]}
+                                label="Age"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value={10}>DDOS</MenuItem>
+                                <MenuItem value={20}>SSH Bruteforce</MenuItem>
+                                <MenuItem value={30}>Man in the Middle</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
 
-                <Button variant="outlined" className="start-button flex-item">Start Attack</Button>
-                <Button variant="outlined" className="stop-button flex-item">Stop Attack</Button>
-                <Button variant="outlined" className="reroll-button flex-item">Reroll Environment</Button>
+                    <div className="lmfao">
+                        <Button variant="outlined" className="start-button flex-item">Start Attack</Button>
+                        <Button variant="outlined" className="stop-button flex-item">Stop Attack</Button>
+                    </div>
+                    <Button variant="outlined" className="reroll-button flex-item">Reroll Environment</Button>
+                </div>
+
             </div>
-            <div className="attack-status">
-                <div className="overview">
-                    <li>
-                        Man in the middle
-                    </li>
+            <div className="form-wrapper">
+                <div className="attack-status">
+                    <div className="overview">
+                        <li>
+                            Man in the middle
+                        </li>
+                    </div>
                 </div>
             </div>
         </div>
